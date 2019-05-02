19e739316c6a4b8b17489ee847e7ef1e0c96204b
Tiana Johnson <tiana.marie013@gmail.com>
2019-05-02 08:27:37 -0500

    comments

--- src/components/CustomerNotesPage/CustomerNotesPage.js
+++ src/components/CustomerNotesPage/CustomerNotesPage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+// This is also seen as the build status page on the client side
 class CustomerNotesPage extends Component {
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_PROJECT', payload: { id: this.props.match.params.id } });      
