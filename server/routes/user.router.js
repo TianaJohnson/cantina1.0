759cd9255c29a05437499aa72ded561dd12d81c3
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-28 20:55:22 -0500

    ...

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -8,9 +8,6 @@ const router = express.Router();
 //Something is wrong with passport
 
 
-
-
-
 // Handles Ajax request for user information if user is authenticated
 router.get('/', rejectUnauthenticated, (req, res) => {
   // Send back user object from the session (previously queried from the database)
