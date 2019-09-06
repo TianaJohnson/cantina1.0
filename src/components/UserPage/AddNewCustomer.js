72af9c1cc9183fcf1356e20beed424584d2da7a6
Tiana Johnson <tiana.marie013@gmail.com>
2019-09-06 07:55:15 -0500

    css organization

--- src/components/UserPage/AddNewCustomer.js
+++ src/components/UserPage/AddNewCustomer.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 //import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import './AddNewCustomer.css';
 
 
 class AddNewCustomer extends Component {
