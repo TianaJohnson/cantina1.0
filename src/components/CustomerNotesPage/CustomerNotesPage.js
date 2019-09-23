59e8640766d69495e90d5bd089d6be66fbf04080
Tiana Johnson <tiana.marie013@gmail.com>
2019-09-22 20:42:26 -0500

    get something donr

--- src/components/CustomerNotesPage/CustomerNotesPage.js
+++ src/components/CustomerNotesPage/CustomerNotesPage.js
@@ -14,7 +14,7 @@ class CustomerNotesPage extends Component {
         alert('Sent!');
         this.props.history.push('/home');
     }
-
+/// you gotta do something soon
     
     render() {
         const project = this.props.reduxStore.project.projectReducer;
